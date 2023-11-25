77eba8f9424e43a81f32341d649f87b2c3764456
Nikolay Petkov <horecabg.info@gmail.com>
2023-11-25 14:51:43 +0200

    Add ClentService class

---
+++ CRMSystem/src/com/sirmaacademy/crm/service/ClientService.java
@@ -0,0 +1,65 @@
+package com.sirmaacademy.crm.service;
+
+import com.sirmaacademy.crm.Client;
+
+public class ClientService implements Service {
+    @Override
+    public void addClient(Client client) {
+
+    }
+
+    @Override
+    public void addClientFile(Client client) {
+
+    }
+
+    @Override
+    public void updateClient(int id, Client client) {
+
+    }
+
+    @Override
+    public void removeClient(int id) {
+
+    }
+
+    @Override
+    public void viewClients() {
+
+    }
+
+    @Override
+    public Client searchById(int id) {
+        return null;
+    }
+
+    @Override
+    public void searchByName(String name) {
+
+    }
+
+    @Override
+    public void searchByIndustry(String industry) {
+
+    }
+
+    @Override
+    public boolean isActive(Client client) {
+        return false;
+    }
+
+    @Override
+    public void closeWriter() {
+
+    }
+
+    @Override
+    public void loadData() {
+
+    }
+
+    @Override
+    public void saveData() {
+
+    }
+}
